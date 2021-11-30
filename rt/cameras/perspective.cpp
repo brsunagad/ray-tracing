b6360ebabcb29a720e11c1a85cf1bc7c32f5b508
brsunagad <brsunagad@gmail.com>
2021-12-01 00:10:27 +0100

    changes to perspective cam

--- rt/cameras/perspective.cpp
+++ rt/cameras/perspective.cpp
@@ -5,20 +5,18 @@ namespace rt {
 PerspectiveCamera::PerspectiveCamera(const Point& center, const Vector& forward, const Vector& up, float verticalOpeningAngle, float horizontalOpeningAngle)
 {
     this->center = center;
-    this->forward = forward;
-    this->up = up;
+    this->forward = forward.normalize();
+    this->up = up.normalize();
     this->verticalOpeningAngle = verticalOpeningAngle;
     this->horizontalOpeningAngle = horizontalOpeningAngle;
-    this->w = -forward.normalize();
-    this->u = cross(up, w).normalize();
-    this->v = cross(w, u).normalize();
-    this->sx = forward.length() * tan(horizontalOpeningAngle / 2);//calculate x-scale
-    this->sy = forward.length() * tan(verticalOpeningAngle / 2);//calculate y-scale
+    this->u = cross(forward, up).normalize();
+    this->v = cross(u, forward).normalize();
+    this->sx = tan(horizontalOpeningAngle / 2);//calculate x-scale
+    this->sy = tan(verticalOpeningAngle / 2);//calculate y-scale
 }
 
 Ray PerspectiveCamera::getPrimaryRay(float x, float y) const {
-   
-
+ 
     return Ray(center, (forward + u * sx * x + v * sy * y).normalize());
 }
 
